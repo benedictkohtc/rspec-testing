140f43ed9832584156aaae3e439e06eca814c26a
benedictkohtc <benedictkohtc@gmail.com>
2017-01-31 17:01:06 +0800

    all tests are passing

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,15 @@
 class Tree
+  attr_accessor :type
+  attr_accessor :height
+  attr_accessor :age
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    @height += water_amount / 10
   end
 end
